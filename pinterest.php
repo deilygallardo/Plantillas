14b5f159ee6787e8dc984513420d705864f35a09
deilygallardo <38663963+deilygallardo@users.noreply.github.com>
2018-07-29 17:45:39 -0500

    Add files via upload

---
+++ pinterest.php
@@ -0,0 +1,132 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Inversiones y Servicios Ydrogo</title>
+	<meta charset="UTF-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	 <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/solar/bootstrap.min.css" rel="stylesheet">
+	 <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	 <link rel="stylesheet" href="css/pinterest.css">
+
+  
+
+</head>
+<body>
+
+   <div class="container-fluid text-center">
+	<div class="row">
+		<section id="demo">
+
+		<article class="white-panel"> <img src="imagenes/color.png" alt="ALT">
+		<h1><a href="detalle1.php">COLORES Y PLUMONES</a></h1>
+		<p>La marca del estudiante</p>
+		<h3>Presio:S/.25.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle1.php">Ver mas</a>
+		</p>
+		</article>
+
+ 		<article class="white-panel"> <img src="imagenes/mochila.png"  alt="ALT">
+		<h1><a href="detalle2.php">MOCHILAS</a></h1>
+		<p>Mejor calidad en Mochilas.</p>
+		<h3>Precio:S/.70.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle2.php">Ver mas</a>
+		</p>
+		</article>
+
+		<article class="white-panel"> <img src="imagenes/juguete.png" alt="ALT">
+		<h1><a href="detalle3.php">JUGUETES</a></h1>
+		<p>Para el niño y la niña de hogar.</p>
+		<h3>Precio:S/.20.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle3.php">Ver mas</a>
+		</p>
+		</article>
+
+		<article class="white-panel"> <img src="imagenes/lapicero.png" alt="ALT">
+		<h1><a href="detalle4.php">LAPICEROS</a></h1>
+		<p>Los mejores lapiceros:</p>
+		<h3>Precio:S/.30.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle4.php">Ver mas</a>
+		</p>
+		</article>
+
+		<article class="white-panel"> <img src="imagenes/cartuchera.png" alt="ALT">
+		<h1><a href="detalle5.php">CARTUCHERAS</a></h1>
+		<p>las cartucheras.</p>
+		<h3>Precio:S/.18.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle5.php">Ver mas</a>
+		</p>
+		</article>
+		<article class="white-panel"> <img src="imagenes/cuaderno1.jpg"  alt="ALT">
+		<h1><a href="detalle6.php">CUADERNOS</a></h1>
+		<p>la mejora variedad en cuadernos</p>
+		<h3>Precio:S/.70.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle6.php">Ver mas</a>
+		</p>
+		</article>
+		
+		<article class="white-panel"> <img src="imagenes/goma1.png" alt="ALT">
+		<h1><a href="detalle7.php">PEGAMENTOS</a></h1>
+		<p>Las mejores gomas.</p>
+		<h3>Precio:S/.20.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle7.php">Ver mas</a>
+		</p>
+		</article>
+
+		<article class="white-panel"> <img src="imagenes/egrapador1.jpg" alt="ALT">
+		<h1><a href="detalle8.php">ENGRAPADORES</a></h1>
+		<p>Los engrapadores de mejor calidad</p>
+		<h3>Precio:S/.20.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle8.php">Ver mas</a>
+		</p>
+		</article>
+
+		<article class="white-panel"> <img src="imagenes/tomatodo1.jpg" alt="ALT">
+		<h1><a href="detalle9.php">TOMATODOS</a></h1>
+		<p>Los modelos de Tomatodos</p>
+		<h3>Precio:S/.20.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle9.php">Ver mas</a>
+		</p>
+		</article>
+		
+		<article class="white-panel"> <img src="imagenes/lonchera1.jpg" alt="ALT">
+		<h1><a href="detalle10.php">LONCHERAS</a></h1>
+		<p>Ofrecemsos las mejores loncheras.</p>
+		<h3>Precio:S/.20.00</h3>
+		<p>
+			<a class="btn btn-primary" href="carrito.php">Comprar</a>
+			<a class="btn btn-danger"  href="detalle10.php">Ver mas</a>
+		</p>
+		</article>
+		</section>
+		</div>
+	</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/pinterest_grid.js"></script>
+<script src="js/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
